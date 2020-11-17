69d0ff039074fd99c79ae4ddc2a03be650d5f696
rafasilveira <oi@rafasilveira.dev>
2020-11-16 22:56:05 -0300

    feat: serial

--- README.md
+++ README.md
@@ -1,13 +1,28 @@
-# Como executar
+# Exercício Java Threads
+
+Exercício para a disciplina de Programação Concorrente. O objetivo é calcular o ganho de performance ao paralelizar a aplicação de um filtro em uma imagem.
 
 ## Serial
+### Uso
+Argumentos:
+- arquivo de entrada
+- arquivo de saida
+- tamanho do filtro (ímpar, entre 3 e 15)
+
+### Argumentos
 ```bash
 javac Serial.java
-java Serial 
+java Serial res/borboleta.bmp out/saida.bmp 13
 ```
-
 ## Threads
+### Argumentos:
+- arquivo de entrada
+- arquivo de saida
+- tamanho do filtro (ímpar, entre 3 e 15)
+- número de threads
+
+### Uso
 ```bash
 javac Threads.java
-java Threads 
+java Threads res/borboleta.bmp out/saida.bmp 13 5
 ```
--- Serial.java
+++ Serial.java
@@ -1,25 +1,25 @@
 import java.awt.Color;
 
-import javax.swing.plaf.ColorUIResource;
+// import javax.swing.plaf.ColorUIResource;
 
 public class Serial {
 
-  public static void main (String[] args) {
-    if ( args.length < 3 ){
-			System.out.printf("Uso: <imagem_de_entrada.bmp> <saida.bmp> <tamanho_filtro> \n");
-			return;
+  public static void main(String[] args) {
+  // public Serial(String[] args) {
+    if (args.length < 3) {
+      System.out.printf("Uso: <imagem_de_entrada.bmp> <saida.bmp> <tamanho_filtro>\n");
+      return;
     }
 
-    if ( Integer.parseInt(args[2]) % 2 == 0 ) {
+    if (Integer.parseInt(args[2]) % 2 == 0) {
       System.out.println("Tamanho do filtro precisa ser ímpar");
       return;
     }
-    
-    if ( Integer.parseInt(args[2]) > 15 ) {
+
+    if (Integer.parseInt(args[2]) > 15) {
       System.out.println("Tamanho do filtro precisa ser menor que 15");
       return;
     }
-
 
     int filterSize = Integer.parseInt(args[2]);
 
@@ -29,51 +29,52 @@ public class Serial {
     System.out.println("Largura da imagem: " + input.width);
     System.out.println("Altura da imagem: " + input.height);
 
+    // int[] filter_buffer_red = new int[15 * 15];
+    // int[] filter_buffer_green = new int[15 * 15];
+    // int[] filter_buffer_blue = new int[15 * 15];
 
-    int[] filter_buffer_red = new int[15*15];
-    int[] filter_buffer_green = new int[15*15];
-    int[] filter_buffer_blue = new int[15*15];
+    int[] filter_buffer_red = new int[filterSize * filterSize];
+    int[] filter_buffer_green = new int[filterSize * filterSize];
+    int[] filter_buffer_blue = new int[filterSize * filterSize];
+
     int filter_reach = filterSize / 2;
 
     int width = input.width;
     int height = input.height;
 
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            int filter_offset = 0;
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        int filter_offset = 0;
 
-            for (int r = -filter_reach; r <= filter_reach; r++)
-            {
-                for (int c = -filter_reach; c <= filter_reach; c++)
-                {
-                    int ii = i + r;
-                    int jj = j + c;
+        for (int r = -filter_reach; r <= filter_reach; r++) {
+          for (int c = -filter_reach; c <= filter_reach; c++) {
+            int ii = i + r;
+            int jj = j + c;
 
-                    if (ii > 0 && ii < height && jj > 0 && jj < width)
-                    {
-                      Color pixel = input.acc(i, j);
-                      filter_buffer_red[filter_offset] = pixel.getRed();
-                      filter_buffer_green[filter_offset] = pixel.getGreen();
-                      filter_buffer_blue[filter_offset] = pixel.getBlue();
-                      filter_offset++;
-                    }
-                }
+            if (ii > 0 && ii < height && jj > 0 && jj < width) {
+              Color pixel = output.acc(ii, jj);
+              filter_buffer_red[filter_offset] = pixel.getRed();
+              filter_buffer_green[filter_offset] = pixel.getGreen();
+              filter_buffer_blue[filter_offset] = pixel.getBlue();
+              filter_offset++;
             }
-
-            int mean_offset = filter_offset / 2;
-            sort_buffer(filter_buffer_red, 0, filter_offset - 1);
-            sort_buffer(filter_buffer_green, 0, filter_offset - 1);
-            sort_buffer(filter_buffer_blue, 0, filter_offset - 1);
-
-            int r = filter_buffer_red[mean_offset];
-            int g = filter_buffer_green[mean_offset];
-            int b = filter_buffer_blue[mean_offset];
-            Color color = new ColorUIResource(r, g, b);
-            output.setPixel(i, j, color);
-
+          }
         }
+
+        int mean_offset = filter_offset / 2;
+        sort_buffer(filter_buffer_red, 0, filter_offset - 1);
+        sort_buffer(filter_buffer_green, 0, filter_offset - 1);
+        sort_buffer(filter_buffer_blue, 0, filter_offset - 1);
+
+        int r = filter_buffer_red[mean_offset];
+        int g = filter_buffer_green[mean_offset];
+        int b = filter_buffer_blue[mean_offset];
+
+        Color color = new Color(r, g, b);
+        // Color color = new Color(0, 0, 0);
+        output.setPixel(i, j, color);
+
+      }
     }
 
     output.ToBmp(args[1]);
@@ -107,6 +108,13 @@ public class Serial {
     buffer[high] = aux;
 
     return i;
+  }
+
+  static void printArray(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.printf(arr[i] + ", ");
+    }
+    System.out.println();
   }
 
 }
